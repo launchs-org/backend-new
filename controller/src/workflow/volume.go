ccc30f050bd1668a873b73da17b5872d6227a341
mattuu0 <tomatomato170@gmail.com>
2026-05-28 02:54:59 +0900

    volume関連の applying を running に変更

--- controller/src/workflow/volume.go
+++ controller/src/workflow/volume.go
@@ -83,8 +83,8 @@ func MountVolumeWorkflow(ctx workflow.Context, input MountVolumeInput) error {
 		return err
 	}
 
-	// applying に変更（Pod が Ready になるまで Watcher が監視して running に遷移）
-	return workflow.ExecuteActivity(ctx, dbAct.DBUpdateContainerStatus, input.ContainerID, "applying").Get(ctx, nil)
+	// running に変更（Pod が Ready になるまで Watcher が監視して running に遷移）
+	return workflow.ExecuteActivity(ctx, dbAct.DBUpdateContainerStatus, input.ContainerID, "running").Get(ctx, nil)
 }
 
 // UnmountVolumeWorkflow はボリュームをアンマウントした状態で Deployment を再 Apply します。
@@ -115,6 +115,6 @@ func UnmountVolumeWorkflow(ctx workflow.Context, input UnmountVolumeInput) error
 		return err
 	}
 
-	// applying に変更（Pod が Ready になるまで Watcher が監視して running に遷移）
-	return workflow.ExecuteActivity(ctx, dbAct.DBUpdateContainerStatus, input.ContainerID, "applying").Get(ctx, nil)
+	// running に変更（Pod が Ready になるまで Watcher が監視して running に遷移）
+	return workflow.ExecuteActivity(ctx, dbAct.DBUpdateContainerStatus, input.ContainerID, "running").Get(ctx, nil)
 }
